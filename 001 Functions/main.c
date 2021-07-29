3faf380b04590baba487a5019d7b2ba3b979f614
yellareddy05 <yella0511@gmail.com>
2021-07-30 05:27:52 +0530

    functions

---
+++ 001 Functions/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 14-Jul-2021
+ *      Author: yellareddy
+ */
+
+#include<stdio.h>
+int main(void)
+{
+	printf("common Kick the ball\n");
+	printf("Press 'Enter' to exit the application\n ");
+	getchar();
+	return 0;
+
+}
+
---
+++ 002 Functions/Debug/fun_def.d
@@ -0,0 +1,1 @@
+fun_def.o: ../fun_def.c
